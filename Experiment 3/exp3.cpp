0115f97441f706d2f036c148c0370ef59020ba0c
vanquisherak <ankitkushwaha9104@gmail.com>
2025-09-18 12:53:19 +0530

    Experiments Done

---
+++ Experiment 3/exp3.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> countFreq(vector<int> &arr) {
+    map<int, int>mp;
+    for(int i = 0; i < arr.size(); i++){
+        mp[arr[i]]++;
+    }
+    vector<vector<int>>ans;
+    for(auto it  : mp){
+        ans.push_back({it.first, it.second});
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> arr = { 1, 2, 3, 4 };
+    vector<vector<int>> ans = countFreq(arr);
+    
+    for(int i = 0; i < ans.size(); i++) {
+        for(int j = 0; j < ans[i].size(); j++) {
+            cout<<ans[i][j]<<' ';
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
